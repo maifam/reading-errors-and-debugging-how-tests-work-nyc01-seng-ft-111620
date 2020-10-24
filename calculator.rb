02c00d9042d9e3e768fa12c434a4a9801ffe8027
Mai Pham <maitphm@gmail.com>
2020-10-24 04:07:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,10 +3,10 @@
 first_number = 5 
 second_number = 7
 
-puts sum = first_number + second_number
+sum = first_number + second_number
 
-puts difference = first_number - second_number 
+difference = first_number - second_number 
 
-puts product = first_number * second_number 
+product = first_number * second_number 
 
-puts quotient = first_number.to_f / second_number 
+quotient = first_number.to_f / second_number 
