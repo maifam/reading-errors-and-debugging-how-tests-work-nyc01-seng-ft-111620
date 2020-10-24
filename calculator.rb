5d6a2136678806c8ac7cfb641f3ee8ec431244e2
Mai Pham <maitphm@gmail.com>
2020-10-24 04:04:43 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,11 @@
 
 first_number = 5 
 second_number = 7
+
+sum = first_number + second_number
+
+difference = first_number - second_number 
+
+product = first_number * second_number 
+
+quotient = first_number.to_f / second_number 
